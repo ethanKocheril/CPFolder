fc83f56585e2be4a80d064c48776bad2bebe4c95
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-03 17:25:35 +0000

    fixed cin

--- paint.cpp
+++ paint.cpp
@@ -4,7 +4,7 @@ int main(void) {
 	freopen("paint.in","r", stdin);	
 	freopen("paint.out", "w", stdout);
 	std::vector<bool> cover(100);
-	int a, b, c, d; cin >> a >> b >> c >> d;
+	int a, b, c, d; std::cin >> a >> b >> c >> d;
 	//for loops modify values in vector from ranges a to b and c to d. Overlaps will automatically be found. 
 	for (int i = a; i < b; ++i) cover[i] = 1;
 	for (int i = c; i < d; ++i) cover[i] = 1;
