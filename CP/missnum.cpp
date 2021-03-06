544991bb388f5459ddf30efeae661976c6c20f7e
ethanKocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-06 13:12:10 +0000

    finished testing it

--- CP/missnum.cpp
+++ CP/missnum.cpp
@@ -1,17 +1,5 @@
+#include <bits/stdc++.h>
 #include <cstdio>
-#include <iostream>
-int main(void) {
-	int n;
-	scanf("%d", &n);
-	int sum = recursiveSum(n);
-	int *v;
-	v = malloc(n * sizeof(int));
-  for(i = 0; i < n; i++)
-    scanf("%d", &v[i]);
-  int sum2 = recursiveSum(v[n - 1]);
-
-  printf("%d", sum - sum2);
-}
 
 int recursiveSum(int n) {
 	if(n == 1) {
@@ -20,3 +8,20 @@ int recursiveSum(int n) {
 
 	return n + recursiveSum(n - 1);
 }
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	int sum = recursiveSum(n);
+	int o;
+	int sum2 = 0;
+
+  for(int i = 0; i < n - 1; i++) {
+    scanf("%d", &o);
+    sum2 += o;
+  }
+  
+  printf("%d\n", sum - sum2);
+}
+
+
