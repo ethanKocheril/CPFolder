3672f79746c3b2b579e896352fa64b0421209e8c
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-10 02:30:56 +0000

    highest is initialized at 1 now.

--- CP/CSES/reps.cpp
+++ CP/CSES/reps.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(void) {
 	string input = "";
 	int count = 1;
-	int highest = 0;
+	int highest = 1;
 	cin >> input;
 	for(int i = 0; i < input.length() - 1; i++) {
 		if(input.at(i) != input.at(i + 1)) {
