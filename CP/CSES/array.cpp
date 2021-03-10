94fd0f8e9f76bc9305a54242b9b085dbdd398017
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-10 03:12:49 +0000

    tried editing

--- CP/CSES/array.cpp
+++ CP/CSES/array.cpp
@@ -5,15 +5,17 @@ int main(void) {
 	int sum = 0;
 	scanf("%d", &n);
 	int arr [n];
+	int diff = 0;
 
 	for(int i = 0; i < n; i++) {
 		std::cin >> arr[i];
 	} 
 	
 	for(int i = 1; i < n; i++) {
-		if(arr[i] - arr[i - 1] < 0) {
-			sum -= arr[i] - arr[i - 1];
-			arr[i] += -1 * arr[i] - arr[i - 1];
+		diff = arr[i - 1] - arr[i];
+		if(diff > 0) {
+			sum += diff;
+			arr[i] += diff;
 		}
 	}
 	printf("%d\n", sum);
