2c9e0cfefbfe22b41b349cf59b861d6b86fdbf6a
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-13 02:37:56 +0000

    tested, tried to fix issues with smaller arrays

--- CP/CSES/array.cpp
+++ CP/CSES/array.cpp
@@ -4,15 +4,16 @@ int main(void) {
 	int n;
 	long long sum = 0;
 	scanf("%d", &n);
-	int arr [n];
+	long long arr [n];
 	long long diff = 0;
 
 	for(int i = 0; i < n; i++) {
 		std::cin >> arr[i];
 	} 
 	
-	for(int i = 0; i < n; i++) {
+	for(int i = 1; i < n; i++) {
 		diff = arr[i - 1] - arr[i];
+		printf("diff is: %lli\n", diff);
 		if(diff > 0) {
 			sum += diff;
 			arr[i] += diff;
