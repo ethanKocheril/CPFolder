5b315e321edf4f896035d004a11ac010709d9256
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-27 02:25:18 +0000

    reworked solution, should work with test case now

--- CP/CSES/permu.cc
+++ CP/CSES/permu.cc
@@ -3,15 +3,16 @@
 int main() {
 	int n;
 	scanf("%d", &n);
-	if(n == 2 || n == 3) {
+	if(n == 1) printf("%d", n);
+	if(n <= 3) {
 		printf("NO SOLUTION\n");
 		return 0;
 	}
-	for(int i = 0; i < n; i+=2) {
-		printf("%d ", n - i);
+	for(int i = 2; i <= n; i+=2) {
+		printf("%d ", i);
 	}
 
-	for(int i = 1; i < n; i+=2) {
-		printf("%d ", n - i);
+	for(int i = 1; i <= n; i+=2) {
+		printf("%d ", i);
 	}
 }
