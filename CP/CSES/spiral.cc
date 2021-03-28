12c09a771af614f6a7c5cd011cf666dbfa0533bb
Ethan Kocheril <2021kocherilethanp@aaps.k12.mi.us>
2021-03-28 20:50:53 +0000

    reorganized usaco directory, attempted spiral problem

--- CP/CSES/spiral.cc
+++ CP/CSES/spiral.cc
@@ -2,11 +2,22 @@
 
 int main(void) {
 	int t; 
-	int spiral[][];
 	scanf("%d", &t);
+	int a[t], b[t];
 	for(int i = 0; i < t; i++) {
-		int y, x;
-		scanf("%d %d", &y, &x);
-		printf("%d", spiral[x][y]);	
+		int *y, *x;
+		y = &b[0];
+		x = &a[0];
+		scanf("%d %d", *(y + i),*(x + i));
+	}
+	int n = sizeof(a);
+	std::sort(a, a+n);
+	std::sort(b, b+n);
+	for(int i = 0; i < n; i++) {
+		printf("%d ", *(b + i));
+	}       		       
+
+	for(int i = 0; i < n; i++) {
+		printf("%d ", *(a + i));
 	}
 }
